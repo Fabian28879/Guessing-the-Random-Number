30dde33211bf20134755a216112f2ac1207ae9a6
Fabian28879 <48481699+Fabian28879@users.noreply.github.com>
2019-03-12 19:37:32 +0100

    Update ZufallszahlErraten.py

--- ZufallszahlErraten.py
+++ ZufallszahlErraten.py
@@ -1,11 +1,11 @@
 from random import randint
-Zufallszahl = randint(0,100)                                                                    #Erstellen der Zufallszahl.
+Zufallszahl = randint(0,100)                                                                    #erstellen der Zufallszahl.
 Eingabe = int(101)                                                                              #Eingabe wird definiert.
-while Eingabe < 0 or Eingabe > 100:                                                             #Anfordern einer Eingabe und Überprüfung, ob die angegebene Zahl den Anforderungen Entspricht.
+while Eingabe < 0 or Eingabe > 100:                                                             #anfordern einer Eingabe und Überprüfung, ob die angegebene Zahl den Anforderungen entspricht.
     Eingabe = input('Geben Sie eine Zahl von 0 - 100 an: ')
-    try:                                                                                        #Umwandeln der Eingabe in das "int" Format und überprüfen, ob eine gültige Eingabe gemacht wurde.
+    try:                                                                                        #umwandeln der Eingabe in das "int" Format und überprüfen, ob eine gültige Eingabe gemacht wurde.
         Eingabe = int(Eingabe)
-    except:                                                                                     #bei einer ungültigen Eingabe wird eine Eingabe außerhalb des Anforderungsbereichen Angegeben, sodass der Benutzer wieder Angefragt wird.
+    except:                                                                                     #bei einer ungültigen Eingabe wird eine Eingabe außerhalb des Anforderungsbereiches angegeben, sodass der Benutzer wieder angefragt wird.
         Eingabe = 101
 while not Eingabe == Zufallszahl:                                                               #Überprüfung, ob die Zufallszahl erraten wurde. Andernfalls wird dem Nutzer die Information gegeben,
     if Eingabe > Zufallszahl:                                                                   #ob sich der angegebene Wert unter oder über dem gesuchten Wert befindet und ein neuer Versuch gefordert.
@@ -19,5 +19,5 @@ while not Eingabe == Zufallszahl:                                               
             Eingabe = int(Eingabe)
         except:
             Eingabe = 101
-print('Herzlichten Glückwunsch, Sie haben die gesuchte Zahl erraten')                           #Sobald die Zufallszahl Erraten wurde, wird dem Nutzer beglückwünscht.
-temp = input('Geben Sie etwas beliebiges an, um das Programm zu beenden: ')                     #Zuletzt kann der Nutzer durch das Angeben von etwas beliebigem oder dem einfachen betätigen der Entertaste das Programm beenden.
+print('Herzlichten Glückwunsch, Sie haben die gesuchte Zahl erraten')                           #sobald die Zufallszahl erraten wurde, wird dem Nutzer beglückwünscht.
+temp = input('Geben Sie etwas beliebiges an, um das Programm zu beenden: ')                     #zuletzt kann der Nutzer durch das Angeben von etwas beliebigem oder dem einfachen betätigen der Entertaste das Programm beenden.
